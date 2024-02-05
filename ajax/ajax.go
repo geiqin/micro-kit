be0c67455c8a065a1aa6ea846502a48497b133fe
tangyonghai <tech@geiqin.com>
2024-02-05 20:27:49 +0800

    modify add ajax

---
+++ ajax/ajax.go
@@ -0,0 +1,21 @@
+package ajax
+
+import (
+	"github.com/geiqin/gotools/helper"
+	"github.com/micro/go-micro/v2/errors"
+)
+
+// 主要针对Service层结果转换为Json数据
+func ResponseData(who interface{}, err error) string {
+	ret := &ResultData{}
+	if err != nil {
+		e := errors.Parse(err.Error())
+		ret.Code = int64(e.Code)
+		ret.Msg = e.Detail
+		ret.Message = e.Detail
+	} else {
+		ret.Code = 1
+		ret.Data = who
+	}
+	return helper.JsonEncode(ret)
+}
---
+++ ajax/models.go
@@ -0,0 +1,41 @@
+package ajax
+
+// 结果数据
+type ResultData struct {
+	Code    int64       `json:"code"`              //错误代码: 成功：1 ，其它数字为失败
+	Data    interface{} `json:"data"`              //成功数据
+	Msg     string      `json:"msg,omitempty"`     //错误消息
+	Message string      `json:"message,omitempty"` //错误消息[兼容旧版本]
+
+}
+
+// 分页数据
+type PageData struct {
+	Pager interface{} `json:"pager,omitempty"`
+	Items interface{} `json:"items"`
+}
+
+// 输出错误json数据
+func Failed(message string, errCode ...int64) *ResultData {
+	var code int64 = 400
+	if errCode != nil {
+		c := errCode[0]
+		if c > 1 {
+			code = c
+		}
+	}
+	ret := &ResultData{
+		Code:    code,
+		Message: message,
+	}
+	return ret
+}
+
+// 输出成功json数据
+func Succeed(data interface{}) *ResultData {
+	ret := &ResultData{
+		Code: 1,
+		Data: data,
+	}
+	return ret
+}
