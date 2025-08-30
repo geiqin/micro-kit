deb5c34e4f5a7532c18caa62f4b7d31ab50c581e
tangyonghai <tech@geiqin.com>
2025-08-30 12:05:35 +0800

    d

--- auth/loadWrapper.go
+++ auth/loadWrapper.go
@@ -7,6 +7,15 @@ import (
 	"github.com/micro/go-micro/v2/server"
 )
 
+var changeKeys = map[string]string{
+	"Auth-Mode":        "mode",
+	"Auth-User-Id":     "user_id",
+	"Auth-Nickname":    "nickname",
+	"Auth-Session-Key": "session_key",
+	"Auth-Platform-Id": "platform_id",
+	"Auth-Shop-Id":     "shop_id",
+}
+
 // AuthWrapper 是一个高阶函数，入参是 ”下一步“ 函数，出参是认证函数
 // 在返回的函数内部处理完认证逻辑后，再手动调用 fn() 进行下一步处理
 // token 是从 consignment-ci 上下文中取出的，再调用 user-service 将其做验证
@@ -18,55 +27,67 @@ func LoadWrapper(fn server.HandlerFunc) server.HandlerFunc {
 			return errors.New("no auth meta-data found in request")
 		}
 
-		mode := meta["Auth-Mode"]
-		userId := meta["Auth-User-Id"]
-		nickname := meta["Auth-Nickname"]
-		sessionKey := meta["Auth-Session-Key"]
-		platformId := meta["Auth-Platform-Id"]
-		storeId := meta["Auth-Store-Id"]
-		realstoreId := meta["Auth-Realstore-Id"]
-		shopId := meta["Auth-Shop-Id"]
-		clientId := meta["Auth-Client-Id"]
-		extends := meta["Auth-Extends"]
-		application := meta["Application"]
-		applicationClientType := meta["Application-Client-Type"]
+		for k, v := range meta {
+			fk, fHas := changeKeys[k]
+			if fHas {
+				ctx = context.WithValue(ctx, fk, v)
+			} else {
+				ctx = context.WithValue(ctx, k, v)
+			}
+		}
 
-		if mode != "" {
-			ctx = context.WithValue(ctx, "mode", mode)
-		}
-		if extends != "" {
-			ctx = context.WithValue(ctx, "extends", extends)
-		}
-		if userId != "" {
-			ctx = context.WithValue(ctx, "user_id", userId)
-		}
-		if nickname != "" {
-			ctx = context.WithValue(ctx, "nickname", nickname)
-		}
-		if storeId != "" {
-			ctx = context.WithValue(ctx, "store_id", storeId)
-		}
-		if realstoreId != "" {
-			ctx = context.WithValue(ctx, "realstore_id", realstoreId)
-		}
-		if platformId != "" {
-			ctx = context.WithValue(ctx, "platform_id", platformId)
-		}
-		if shopId != "" {
-			ctx = context.WithValue(ctx, "shop_id", shopId)
-		}
-		if sessionKey != "" {
-			ctx = context.WithValue(ctx, "session_key", sessionKey)
-		}
-		if clientId != "" {
-			ctx = context.WithValue(ctx, "client_id", clientId)
-		}
-		if application != "" {
-			ctx = context.WithValue(ctx, "application", application)
-		}
-		if applicationClientType != "" {
-			ctx = context.WithValue(ctx, "application_client_type", applicationClientType)
-		}
+		/*
+			mode := meta["Auth-Mode"]
+			userId := meta["Auth-User-Id"]
+			nickname := meta["Auth-Nickname"]
+			sessionKey := meta["Auth-Session-Key"]
+			platformId := meta["Auth-Platform-Id"]
+			storeId := meta["Auth-Store-Id"]
+			realstoreId := meta["Auth-Realstore-Id"]
+			shopId := meta["Auth-Shop-Id"]
+			clientId := meta["Auth-Client-Id"]
+			extends := meta["Auth-Extends"]
+			application := meta["Application"]
+			applicationClientType := meta["Application-Client-Type"]
+
+			if mode != "" {
+				ctx = context.WithValue(ctx, "mode", mode)
+			}
+			if extends != "" {
+				ctx = context.WithValue(ctx, "extends", extends)
+			}
+			if userId != "" {
+				ctx = context.WithValue(ctx, "user_id", userId)
+			}
+			if nickname != "" {
+				ctx = context.WithValue(ctx, "nickname", nickname)
+			}
+			if storeId != "" {
+				ctx = context.WithValue(ctx, "store_id", storeId)
+			}
+			if realstoreId != "" {
+				ctx = context.WithValue(ctx, "realstore_id", realstoreId)
+			}
+			if platformId != "" {
+				ctx = context.WithValue(ctx, "platform_id", platformId)
+			}
+			if shopId != "" {
+				ctx = context.WithValue(ctx, "shop_id", shopId)
+			}
+			if sessionKey != "" {
+				ctx = context.WithValue(ctx, "session_key", sessionKey)
+			}
+			if clientId != "" {
+				ctx = context.WithValue(ctx, "client_id", clientId)
+			}
+			if application != "" {
+				ctx = context.WithValue(ctx, "application", application)
+			}
+			if applicationClientType != "" {
+				ctx = context.WithValue(ctx, "application_client_type", applicationClientType)
+			}
+
+		*/
 
 		//继续执行下一步处理
 		err := fn(ctx, req, resp)
